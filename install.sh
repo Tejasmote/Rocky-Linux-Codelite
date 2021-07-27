01a0dec5ae4e048ab44ecd020460fd0b7bb5bcdb
Tejas Dattatray Mote <51282371+Tejasmote@users.noreply.github.com>
2021-07-27 21:24:00 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 echo "Codelite installation...."
-sudo yum install wget
+sudo yum install wget cmake
 sudo yum install wxGTK3.x86_64 libsqlite3x-devel.x86_64 libsqlite3x.x86_64 libsq3-devel.x86_64 wxGTK3-gl.x86_64 wxGTK3-i18n.noarch wxGTK3-media.x86_64 wxGTK3-webview.x86_64
 sudo yum install https://download-ib01.fedoraproject.org/pub/epel/8/Everything/x86_64/Packages/w/wxGTK3-devel-3.0.4-11.el8.x86_64.rpm
 sudo dnf install libssh-devel
